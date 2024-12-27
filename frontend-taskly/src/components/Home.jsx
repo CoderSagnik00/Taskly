b6198b95fb687191344e6c99f930343630854107
CoderSagnik00 <sagnikroycode@gmail.com>
2024-12-28 01:16:42 +0530

    func deleteTodo created
    func deleteTodo is created to update the ui with use of setTodos
    useState hook and also No todos available is shown now when there is now
    todo show in the ui

--- frontend-taskly/src/components/Home.jsx
+++ frontend-taskly/src/components/Home.jsx
@@ -3,43 +3,39 @@ import AddTodo from "./AddTodo"
 import Todos from "./Todos"
 
 function Home() {
-    const [todos, setTodos] = useState([
-        {
-            _id: 1,
-            title: "Learn Nodejs",
-            desc: "Learn React development"
-        },
-        {
-            _id: 2,
-            title: "Learn Express",
-            desc: "Learn React development"
-        },
-        {
-            _id: 3,
-            title: "Learn React",
-            desc: "Learn React development"
-        },
-        {
-            _id: 4,
-            title: "Learn MongoDB",
-            desc: "Learn React development"
-        }
-    ])
+    const [todos, setTodos] = useState([])
 
     const addTodo = (e) => {
+        let _id;
+        if (todos.length === 0) {
+             _id = 0;
+        } else {
+             _id = todos[todos.length - 1]._id + 1;
+        }
+
+        if(!e.title || !e.desc){
+            alert("Title or description can not be empty")
+            return;
+        }
 
         const data = {
-            _id: todos[todos.length - 1]._id + 1,
+            _id: _id,
             title: e.title,
             desc: e.desc
         };
-        
+
         setTodos([...todos, data])
+    }
+
+    const delTodo = ({ _id }) => {
+
+        setTodos(todos.filter((e) => e._id !== _id))
+
     }
     return (
         <>
             <AddTodo addTodo={addTodo} />
-            <Todos todos={todos} />
+            <Todos todos={todos} delTodo={delTodo} />
         </>
     )
 }
--- frontend-taskly/src/components/Todos.jsx
+++ frontend-taskly/src/components/Todos.jsx
@@ -2,17 +2,19 @@ import '../css/Todos.css'
 import TodoItem from './TodoItem'
 import PropTypes from 'prop-types'
 
-function Todos({ todos }) {
+function Todos({ todos, delTodo }) {
   return (
     <div className='mx-10 space-y-1'>
       <h1 className='font-bold text-2xl'>Todos</h1>
       <div className='space-y-3'>
-        {todos.map((e) => {
-          return (
-              <TodoItem key={e._id} title={e.title} desc={e.desc} />
-          );
+        {todos.length === 0 ? <h1 className='mt-5 text-center font-semibold'>No Todos to display</h1> :
+          todos.map((e) => {
+            return (
+              <TodoItem key={e._id} _id={e._id} title={e.title} desc={e.desc} delTodo={delTodo} />
+            );
 
-        })}
+          })
+        }
       </div>
     </div>
   )
@@ -25,7 +27,8 @@ Todos.propTypes = {
       title: PropTypes.string,
       desc: PropTypes.string
     })
-  )
+  ),
+  delTodo: PropTypes.func
 }
 
 export default Todos
