183e90bc5cbb516129adc45d5c896be426acf228
CoderSagnik00 <sagnikroycode@gmail.com>
2024-12-28 22:15:04 +0530

    Made the prod enviorment setup some ENV checking

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,28 @@
 import express from 'express'
 import routes_todo from './routes/todo.routes.js'
 import cors from 'cors'
+import path from 'path'
 import 'dotenv/config'
+
 import { connectDB } from './config/db.js';
 
 const app = express();
 
 
-app.use(express.json())
+if (process.env.ENV === 'dev') {
+    app.use(express.json())
+} else if (process.env.ENV === 'prod') {
+    app.use(express.static(path.resolve(import.meta.dirname, "../frontend-taskly/dist")))
 
+}
 
 app.use(cors())
 
 app.use("/api/todo/", routes_todo);
 
-app.listen(5000, () => {
-    console.log("Server started at port 5000...");
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}...`);
     connectDB();
-});
+})
